9a6afb52b3f8c5e6a396fc6bc10c5612e7290fe0
Alejandro Vargas <alejovastillo@gmail.com>
2024-10-04 21:45:22 -0500

    Se uso slick para el carousel me tire el que hacia manual

--- mytinerary/src/Pages/Home.jsx
+++ mytinerary/src/Pages/Home.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import CarouselDefault from '../Components/Carousel'
 
 export default function Home() {
     return (
-        <div>Home</div>
+        <CarouselDefault></CarouselDefault>
     )
 }
