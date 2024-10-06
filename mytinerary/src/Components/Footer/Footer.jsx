c8c24d1049ee9645d67a6e67957d0a7568f2dd6b
David Bedoya <alejovastillo@gmail.com>
2024-10-05 23:36:45 -0500

    Hero header

--- mytinerary/src/Components/Footer/Footer.jsx
+++ mytinerary/src/Components/Footer/Footer.jsx
@@ -3,45 +3,46 @@ import instagram from '../../assets/instagram.svg';
 import twitter from '../../assets/twitter.svg';
 export default function Footer() {
   return (
-    <div className='flex justify-around flex-wrap bg-slate-900 py-2 backdrop-blur-md bg-gray-900/80'>
-      <div className='flex flex-col justify-center mx-2 items-center'>
-        <a className='rounded-lg px-3 hover:bg-slate-600 text-gray-300' href=''>
-          Home
-        </a>
-        <a className='rounded-lg px-3 hover:bg-slate-600 text-gray-300' href=''>
-          Cities
-        </a>
-        <a className='rounded-lg px-3 hover:bg-slate-600 text-gray-300' href=''>
-          Login
-        </a>
-      </div>
-
-      <div className='flex flex-col justify-center mx-2 items-center flex-wrap '>
-        <div className='flex flex-row items-center rounded-lg px-3 hover:bg-slate-600'>
-          <a href='' target='_blank' rel='noopener noreferrer'>
-            <img className='w-6 h-6 me-1' src={facebook} alt='' />
+    <>
+      <div className='flex flex-col items-center justify-around flex-wrap bg-slate-900 py-2 backdrop-blur-md bg-gray-900/80 text-gray-300'>
+        <div className='flex justify-center mx-2 items-center'>
+          <a className='rounded-lg px-3 hover:bg-slate-600' href=''>
+            Home
           </a>
-          <p>Facebook</p>
-        </div>
-        <div className='flex flex-row items-center rounded-lg px-3 hover:bg-slate-600'>
-          <a href='' target='_blank' rel='noopener noreferrer'>
-            <img src={instagram} className='w-6 h-6 me-1' alt='' />
+          <a className='rounded-lg px-3 hover:bg-slate-600' href=''>
+            Cities
           </a>
-          <p className=''>Instagram</p>
-        </div>
-        <div className='flex flex-row items-center rounded-lg px-3 hover:bg-slate-600'>
-          <a href='' target='_blank' rel='noopener noreferrer'>
-            <img src={twitter} className='w-6 h-6 me-1' alt='' />
+          <a className='rounded-lg px-3 hover:bg-slate-600' href=''>
+            Login
           </a>
-          <p className=''>Twitter</p>
+        </div>
+        <div className='flex justify-center mx-2 items-center flex-wrap  '>
+          <div className='flex flex-row items-center rounded-lg px-3 hover:bg-slate-600'>
+            <a href='' target='_blank' rel='noopener noreferrer'>
+              <img className='w-6 h-6 me-1' src={facebook} alt='' />
+            </a>
+            <p>Facebook</p>
+          </div>
+          <div className='flex flex-row items-center rounded-lg px-3 hover:bg-slate-600'>
+            <a href='' target='_blank' rel='noopener noreferrer'>
+              <img src={instagram} className='w-6 h-6 me-1' alt='' />
+            </a>
+            <p className=''>Instagram</p>
+          </div>
+          <div className='flex flex-row items-center rounded-lg px-3 hover:bg-slate-600'>
+            <a href='' target='_blank' rel='noopener noreferrer'>
+              <img src={twitter} className='w-6 h-6 me-1' alt='' />
+            </a>
+            <p className=''>Twitter</p>
+          </div>
+        </div>
+        <div className='flex  items-center gap-2'>
+          <p>Contact Us</p>
+          <p>742 de Evergreen Terrace</p>
+          <p>Email: Contact@mytinerary.com</p>
+          <p>Phone: (123) 390-7809</p>
         </div>
       </div>
-      <div className='flex flex-col items-center mx-2'>
-        <p>Contact Us</p>
-        <p>742 de Evergreen Terrace</p>
-        <p>Email: Contact@mytinerary.com</p>
-        <p>Phone: (123) 390-7809</p>
-      </div>
-    </div>
+    </>
   );
 }
--- mytinerary/src/Pages/Home.jsx
+++ mytinerary/src/Pages/Home.jsx
@@ -1,9 +1,10 @@
 import Slider from '../Components/Slider/Slider';
-
+import Hero from '../Components/Hero/Hero';
 import {data} from '../MockData/Data';
 export default function Home() {
   return (
     <div className='px-4'>
+      <Hero />
       <Slider data={data} />
     </div>
   );
